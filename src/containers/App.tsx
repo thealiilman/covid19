da654a70c6117759c8ddb0d80a6235f997b990fd
thealiilman <aliilman532@gmail.com>
2020-03-25 21:37:01 +0800

    Removed certain template code and modified codebase's structure.

---
+++ src/containers/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>COVID-19 Statistics</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'styles/index.css';
+import App from 'containers/App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
